5a5fb62ff6724530a0627eb5c41384a7f2b4fc0b
Vaishakh Menon <vaishakhhm@gmail.com>
2021-04-04 11:39:56 +0530

    Initial commit.

---
+++ build/CMakeFiles/network-packet-generator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/network-packet-generator"
+  "../bin/network-packet-generator.pdb"
+  "CMakeFiles/network-packet-generator.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/network-packet-generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/darkangel/Projects/C++/network-packet-generator
+# Build directory: /home/darkangel/Projects/C++/network-packet-generator/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
